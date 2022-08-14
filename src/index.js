aba172de94be002b7d8e0e01896809f41b737f48
YevheniiGrabina <89870811+YevheniiGrabina@users.noreply.github.com>
2022-08-14 15:22:50 +0300

    v3

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './sass/main.scss';
 
-import { refs } from './js/refs';
-import { onFaceBtnClick } from './js/renderCategoty';
+// import { refs } from './js/refs';
+// import { onFaceBtnClick } from './js/renderCategoty';
 
-refs.face.addEventListener('click', onFaceBtnClick);
+// refs.face.addEventListener('click', onFaceBtnClick);
 
 
 
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,17 +1,17 @@
-export const refs = {
-    bodyEl: document.querySelector('body'),
-    face: document.querySelector('.js-nav-face-btn'),
-    shopSection: document.querySelector('.shop-categories '),
-    newSection: document.querySelector('.new-shop-category'),
-    liderSection:document.querySelector('.lider-shop-category'),
-    faceSection: document.querySelector('.for-face'),
-    logo: document.querySelector('.js-logo-btn'),
-    header: document.querySelector('.header'),
-    formEl: document.querySelector('.form'),
-    formInputEl: document.querySelector('.form__input'),
-    footer: document.querySelector('.footer'),
-    main: document.querySelector('.main'),
+// export const refs = {
+//     bodyEl: document.querySelector('body'),
+//     face: document.querySelector('.js-nav-face-btn'),
+//     shopSection: document.querySelector('.shop-categories '),
+//     newSection: document.querySelector('.new-shop-category'),
+//     liderSection:document.querySelector('.lider-shop-category'),
+//     faceSection: document.querySelector('.for-face'),
+//     logo: document.querySelector('.js-logo-btn'),
+//     header: document.querySelector('.header'),
+//     formEl: document.querySelector('.form'),
+//     formInputEl: document.querySelector('.form__input'),
+//     footer: document.querySelector('.footer'),
+//     main: document.querySelector('.main'),
 
 
     
-}
+// }
