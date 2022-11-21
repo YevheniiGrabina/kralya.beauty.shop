f5c477c91b58d99932f5f090a3a5f747af0000ec
YevheniiGrabina <89870811+YevheniiGrabina@users.noreply.github.com>
2022-11-21 16:19:58 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,6 @@ import './js/slider.js';
 import { refs } from './js/refs';
 
 
-import { onHairBtnClick } from './js/hairCategory';
-import { onBoxBtnClick } from './js/boxCategory';
-import { onGiftsBtnClick } from './js/giftsCategory';
-
-
-refs.hair.addEventListener('click', onHairBtnClick);
-refs.box.addEventListener('click', onBoxBtnClick);
-refs.gifts.addEventListener('click', onGiftsBtnClick);
 
 
 
