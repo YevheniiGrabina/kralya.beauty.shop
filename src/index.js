96624d5587d171f6f858bf8f8e56ffdf04a20c27
YevheniiGrabina <89870811+YevheniiGrabina@users.noreply.github.com>
2022-11-21 15:39:10 +0200

    slider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import './sass/main.scss';
+import './js/mobile-menu.js';
+import './js/modal.js';
+import './js/slider.js';
 
-
-
-
-
-import { refs } from './js/refs';
+import {refs} from './js/refs'
 import { onFaceBtnClick } from './js/renderFaceCategory';
 import { onBodyBtnClick } from './js/renderBodyCategory';
 import { onLipsBtnClick } from './js/renderLipsCategory';
