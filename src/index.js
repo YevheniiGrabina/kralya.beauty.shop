8fc75d6529edc3c8d7a6f1d7b46ab7571778fd7b
YevheniiGrabina <89870811+YevheniiGrabina@users.noreply.github.com>
2022-11-21 15:49:38 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './js/modal.js';
 import './js/slider.js';
 
 import { refs } from './js/refs';
-import { onFaceBtnClick } from './js/renderFaceCategory';
-import { onBodyBtnClick } from './js/renderBodyCategory';
-import { onLipsBtnClick } from './js/renderLipsCategory';
-import { onHairBtnClick } from './js/hairCategory';
-import { onBoxBtnClick } from './js/boxCategory';
-import { onGiftsBtnClick } from './js/giftsCategory';
+import  onFaceBtnClick  from './js/renderFaceCategory';
+import  onBodyBtnClick  from './js/renderBodyCategory';
+import  onLipsBtnClick  from './js/renderLipsCategory';
+import  onHairBtnClick  from './js/hairCategory';
+import  onBoxBtnClick  from './js/boxCategory';
+import  onGiftsBtnClick  from './js/giftsCategory';
 
 refs.face.addEventListener('click', onFaceBtnClick);
 refs.body.addEventListener('click', onBodyBtnClick);
